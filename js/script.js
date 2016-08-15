496ff9b3556334ab201e4df3378920b29b9a6ce0
HK Kahng and Jonathan Toler <hk@>
2016-08-15 10:14:07 -0700

    add code to collect input, reorder lists, and populate unordered list on page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,30 @@
 // Front End Code
 $(document).ready(function() {
 
+  $("#blanks form").submit(function(event) {
+    var yourFavs = [];
+    var newFavs =[];
+    var yrList = [];
+    var j = 0;
+    $("#yourList").empty();
+    for (var i = 1; i < 6; i += 1) {
+      var favInput = "#favThing" + i;
+      yourFavs.push($(favInput).val());
+    };
+    newFavs =[yourFavs[1],yourFavs[0],yourFavs[2]];
+    yourFavs.forEach(function(yourFav) {
+      newFavs.push(yourFavs[j]);
+      j++;
+    });
+    // console.log(newFavs);
+    newFavs.forEach(function(newFav) {
+      item = "<li>" + newFav + "</li>";
+      $("#yourList").prepend(item);
+    });
+    // console.log(yrList.join());
+    // $("#yourList").text(yrList.join());
+    event.preventDefault();
+  });
 
 
 
